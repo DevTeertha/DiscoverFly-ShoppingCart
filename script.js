66a871195dec011b039e23dd2c610d215149c1fa
Dev Teertha <devteertha28@gmail.com>
2021-01-30 19:25:51 +0600

    Confirmation Message Added

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function handleProductChange(productId, isPlus) {
     if (productId == 'first-class') {
         productTotal = productNewNumber * 150;
         setDisplay(productId, 'confirm-first-class-quantity');
-        calculationDisplay('confirm-first-class-total' , productTotal);
+        calculationDisplay('confirm-first-class-total', productTotal);
     }
     if (productId == 'economy-input') {
         productTotal = productNewNumber * 100;
         setDisplay(productId, 'confirm-economy-quantity');
-        calculationDisplay('confirm-economy-total',productTotal);
+        calculationDisplay('confirm-economy-total', productTotal);
     }
     subTotal();
 }
@@ -53,30 +53,45 @@ function vat10(vat) {
 function total(subtotal, vat) {
     var totalPrice = subtotal + vat;
     document.getElementById('total').innerText = totalPrice;
-    calculationDisplay('confirm-subtotal' , subtotal);
-    calculationDisplay('confirm-vat' , vat);
-    calculationDisplay('confirm-total' , totalPrice);
+    calculationDisplay('confirm-subtotal', subtotal);
+    calculationDisplay('confirm-vat', vat);
+    calculationDisplay('confirm-total', totalPrice);
 }
 
-
+function checkCondition(id) {
+    if (document.getElementById(id).value == '' || document.getElementById(id).value == null) {
+        return true;
+    }
+    else if(document.getElementById(id).value == '0' || document.getElementById(id).value == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 /* Output Showing */
 function setDisplay(id, setValue) {
     document.getElementById(setValue).innerText = document.getElementById(id).value;
 }
-function calculationDisplay(id , value){
-    document.getElementById(id).innerText = '$'+value;
+function calculationDisplay(id, value) {
+    document.getElementById(id).innerText = '$' + value;
 }
 function Display() {
-    document.getElementById('front-display').style.display = 'none';
-    document.getElementById('confirm-msg').style.display = 'block';
+    if (checkCondition('fly-from') || checkCondition('fly-to') || checkCondition('departure-date') || checkCondition('return-date')){
+        alert('Please Fill The All Info');
+    }
+    else if(checkCondition('first-class') && checkCondition('economy-input')){
+        alert('Please Fill The All Info');
+    }
+    else {
+        document.getElementById('front-display').style.display = 'none';
+        document.getElementById('confirm-msg').style.display = 'block';
 
-    setDisplay('fly-from', 'confirm-fly-from');
-    setDisplay('fly-to', 'confirm-fly-to');
-    setDisplay('departure-date', 'confirm-departure-date');
-    setDisplay('return-date', 'confirm-return-date');
-
-    const firstClassQuantity = document.getElementById('first-class').value;
-    const economyQuantity = document.getElementById('economy-input');
+        setDisplay('fly-from', 'confirm-fly-from');
+        setDisplay('fly-to', 'confirm-fly-to');
+        setDisplay('departure-date', 'confirm-departure-date');
+        setDisplay('return-date', 'confirm-return-date');
+    }
 }
 document.getElementById('book-now-btn').addEventListener('click', Display);
